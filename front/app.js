ada133915d043b29e16a428c661b1d5ba9677c15
usuariocoursera1 <jp.reyes39@uniandes.edu.co>
2021-01-29 07:53:26 -0500

    Cambiar el correo

--- front/app.js
+++ front/app.js
@@ -82,8 +82,8 @@ function processFiles(selectedHTMLFile, selectedCSSFile) {
           document.getElementById("general-errors").hidden = false;
           document.getElementById(
             "general-errors"
-          ).innerHTML = `<p>Ocurrió un error durante la validación: ${error}</p><p>Por favor póngase en contacto con Juan Pablo Reyes al correo jp.reyes39@uniandes.edu.co, 
-            quién para que le indique los pasos a seguir. </p>`;
+          ).innerHTML = `<p>Ocurrió un error durante la validación: ${error}</p><p>Por favor póngase envie un correo a misovirtual-isw@uniandes.edu.co, 
+            para que le indique los pasos a seguir. </p>`;
         })
         .then((res) => {
           renderResponse(
