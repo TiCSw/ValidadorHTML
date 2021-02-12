d47aeda0e1a5e438ca5e30c752eec74b3e2c26a7
usuariocoursera1 <jp.reyes39@uniandes.edu.co>
2021-02-12 17:25:49 -0500

    merged with master

--- front/app.js
+++ front/app.js
@@ -6,9 +6,7 @@ const key = url.searchParams.get("key");
 initializeDivs();
 
 function initializeDivs() {
-  if (type === "1") {
-    document.getElementById("formType2").hidden = true;
-  }
+  document.getElementById("formType2").hidden = type === "1" ? true : false;
   document.getElementById("validation-html").hidden = false;
   document.getElementById("validation-css").hidden = true;
   document.getElementById("details-html").hidden = false;
@@ -33,7 +31,6 @@ document.getElementById("fileForm").onsubmit = function (event) {
   document.getElementById("form-container").hidden = true;
   const selectedHTMLFile = document.getElementById("fileHTML").files[0];
   const selectedCSSFile = document.getElementById("fileCSS").files[0];
-
   processFiles(selectedHTMLFile, selectedCSSFile);
 };
 
@@ -81,15 +78,19 @@ function processFiles(selectedHTMLFile, selectedCSSFile) {
             para que le indique los pasos a seguir. </p>`;
         })
         .then((res) => {
+          console.log(res);
           renderResponse(
             res.isHtmlValid,
+            res.isHtmlWellFormed,
             res.differencesHTML,
             res.isCSSValid,
             res.differencesCSS,
             res.grade
           );
-          drawTree(res.htmlInputStructure, "#source");
-          drawTree(res.htmlExpectedStructure, "#target");
+          if (res.isHtmlWellFormed) {
+            drawTree(res.htmlInputStructure, "#source");
+            drawTree(res.htmlExpectedStructure, "#target");
+          }
         });
     });
   });
@@ -108,6 +109,7 @@ function returnFile(file, callback) {
 
 function renderResponse(
   isHtmlValid,
+  isHtmlWellFormed,
   differencesHTML,
   isCSSValid,
   differencesCSS,
@@ -125,14 +127,28 @@ function renderResponse(
   } else {
     document.getElementById("details-html").hidden = false;
     document.getElementById("errors-html").hidden = false;
-    document.getElementById(
-      "structureResult-html"
-    ).innerHTML = `<p>La estructura del archivo html proporcionado no es válida</p><p>Nota: ${grade}</p>`;
-    document.getElementById("structureResult-html").className = "text-danger";
 
-    document.getElementById("errors-html-list").innerHTML = differencesHTML
-      .map((e) => `<li>${e.message} ${e.value} </li>`)
-      .join("");
+    if (!isHtmlWellFormed) {
+      document.getElementById("well-formed").hidden = true;
+      document.getElementById("errors-html-list").innerHTML =
+        "<p>El archivo proporcionado no es un documento HTML válido.</p>";
+      document.getElementById(
+        "errors-html-list"
+      ).innerHTML += differencesHTML
+        .map((e) => `<li>${e.value} </li>`)
+        .join("");
+    } else {
+      document.getElementById(
+        "structureResult-html"
+      ).innerHTML = `<p>La estructura del archivo html proporcionado no es válida</p><p>Nota: ${grade}</p>`;
+      document.getElementById("structureResult-html").className = "text-danger";
+
+      document.getElementById(
+        "errors-html-list"
+      ).innerHTML = differencesHTML
+        .map((e) => `<li>${e.message} ${e.value} </li>`)
+        .join("");
+    }
   }
 
   if (isCSSValid) {
@@ -148,164 +164,5 @@ function renderResponse(
     document.getElementById("errors-css-list").innerHTML = differencesCSS
       .map((e) => `<li>${e}</li>`)
       .join("");
-  }
-}
-
-function drawTree(treeData, div) {
-  /************** Generate the tree diagram	 *****************/
-  var margin = { top: 20, right: 120, bottom: 20, left: 120 },
-    width = 800 - margin.right - margin.left,
-    height = 500 - margin.top - margin.bottom;
-
-  var i = 0,
-    duration = 750,
-    root;
-
-  var tree = d3.layout.tree().size([height, width]);
-
-  var diagonal = d3.svg.diagonal().projection(function (d) {
-    return [d.y, d.x];
-  });
-
-  var svg = d3
-    .select(div)
-    .append("svg")
-    .attr("width", width + margin.right + margin.left)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  //root = treeData[0];
-  root = treeData;
-  root.x0 = height / 2;
-  root.y0 = 0;
-
-  update(root);
-
-  d3.select(self.frameElement).style("height", "500px");
-
-  function update(source) {
-    // Compute the new tree layout.
-    var nodes = tree.nodes(root).reverse(),
-      links = tree.links(nodes);
-
-    // Normalize for fixed-depth.
-    nodes.forEach(function (d) {
-      d.y = d.depth * 180;
-    });
-
-    // Update the nodes…
-    var node = svg.selectAll("g.node").data(nodes, function (d) {
-      return d.id || (d.id = ++i);
-    });
-
-    // Enter any new nodes at the parent's previous position.
-    var nodeEnter = node
-      .enter()
-      .append("g")
-      .attr("class", "node")
-      .attr("transform", function (d) {
-        return "translate(" + source.y0 + "," + source.x0 + ")";
-      })
-      .on("click", click);
-
-    nodeEnter
-      .append("circle")
-      .attr("r", 1e-6)
-      .style("fill", function (d) {
-        return d._children ? "lightsteelblue" : "#fff";
-      });
-
-    nodeEnter
-      .append("text")
-      .attr("x", function (d) {
-        return d.children || d._children ? -13 : 13;
-      })
-      .attr("dy", ".35em")
-      .attr("text-anchor", function (d) {
-        return d.children || d._children ? "end" : "start";
-      })
-      .text(function (d) {
-        return d.tag || d.node;
-      })
-      .style("fill-opacity", 1e-6);
-
-    // Transition nodes to their new position.
-    var nodeUpdate = node
-      .transition()
-      .duration(duration)
-      .attr("transform", function (d) {
-        return "translate(" + d.y + "," + d.x + ")";
-      });
-
-    nodeUpdate
-      .select("circle")
-      .attr("r", 10)
-      .style("fill", function (d) {
-        return d._children ? "lightsteelblue" : "#fff";
-      });
-
-    nodeUpdate.select("text").style("fill-opacity", 1);
-
-    // Transition exiting nodes to the parent's new position.
-    var nodeExit = node
-      .exit()
-      .transition()
-      .duration(duration)
-      .attr("transform", function (d) {
-        return "translate(" + source.y + "," + source.x + ")";
-      })
-      .remove();
-
-    nodeExit.select("circle").attr("r", 1e-6);
-
-    nodeExit.select("text").style("fill-opacity", 1e-6);
-
-    // Update the links…
-    var link = svg.selectAll("path.link").data(links, function (d) {
-      return d.target.id;
-    });
-
-    // Enter any new links at the parent's previous position.
-    link
-      .enter()
-      .insert("path", "g")
-      .attr("class", "link")
-      .attr("d", function (d) {
-        var o = { x: source.x0, y: source.y0 };
-        return diagonal({ source: o, target: o });
-      });
-
-    // Transition links to their new position.
-    link.transition().duration(duration).attr("d", diagonal);
-
-    // Transition exiting nodes to the parent's new position.
-    link
-      .exit()
-      .transition()
-      .duration(duration)
-      .attr("d", function (d) {
-        var o = { x: source.x, y: source.y };
-        return diagonal({ source: o, target: o });
-      })
-      .remove();
-
-    // Stash the old positions for transition.
-    nodes.forEach(function (d) {
-      d.x0 = d.x;
-      d.y0 = d.y;
-    });
-  }
-
-  // Toggle children on click.
-  function click(d) {
-    if (d.children) {
-      d._children = d.children;
-      d.children = null;
-    } else {
-      d.children = d._children;
-      d._children = null;
-    }
-    update(d);
   }
 }
